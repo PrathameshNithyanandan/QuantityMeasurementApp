f947c55284dccd14e54d66c9fa90edbd4b6f7cc5
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-05-06 09:25:58 +0530

    UC2: Feet and Inches Measurement Equality
    Added functionality to compare lengths in feet and inches.

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,23 +1,32 @@
 public class QuantityMeasurementApp {
 
     // UC1: Feet Measurement Equality
-    // Checks equality of two numerical values measured in feet
     public boolean areFeetEqual(double feet1, double feet2) {
                 return Double.compare(feet1, feet2) == 0;
+    }
+
+    // UC2: Feet and Inches Measurement Equality
+    // 1 foot = 12 inches; converts all to inches for comparison
+    public double feetToInches(double feet) {
+                return feet * 12.0;
+    }
+
+    public boolean areLengthsEqual(double value1, String unit1, double value2, String unit2) {
+                double inches1 = unit1.equalsIgnoreCase("feet") ? feetToInches(value1) : value1;
+                double inches2 = unit2.equalsIgnoreCase("feet") ? feetToInches(value2) : value2;
+                return Double.compare(inches1, inches2) == 0;
     }
 
     public static void main(String[] args) {
                 QuantityMeasurementApp app = new QuantityMeasurementApp();
 
             // UC1: Feet Measurement Equality
-            double feet1 = 3.0;
-                double feet2 = 3.0;
-                boolean result = app.areFeetEqual(feet1, feet2);
-                System.out.println("Are " + feet1 + " feet and " + feet2 + " feet equal? " + result);
+            System.out.println("UC1: Are 3.0 feet and 3.0 feet equal? " + app.areFeetEqual(3.0, 3.0));
+                System.out.println("UC1: Are 2.0 feet and 5.0 feet equal? " + app.areFeetEqual(2.0, 5.0));
 
-            double feet3 = 2.0;
-                double feet4 = 5.0;
-                boolean result2 = app.areFeetEqual(feet3, feet4);
-                System.out.println("Are " + feet3 + " feet and " + feet4 + " feet equal? " + result2);
+            // UC2: Feet and Inches Measurement Equality
+            System.out.println("UC2: Are 1.0 feet and 12.0 inches equal? " + app.areLengthsEqual(1.0, "feet", 12.0, "inches"));
+                System.out.println("UC2: Are 2.0 feet and 24.0 inches equal? " + app.areLengthsEqual(2.0, "feet", 24.0, "inches"));
+                System.out.println("UC2: Are 1.0 feet and 1.0 inches equal? " + app.areLengthsEqual(1.0, "feet", 1.0, "inches"));
     }
 }
