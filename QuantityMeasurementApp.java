ea4246212f2ca613ac8e1d27b5b2117e42bb8b00
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-05-06 09:43:17 +0530

    UC8: Refactoring Unit Enum to Standalone
    Refactored Quantity class to use LengthUnit enum for unit representation and conversion. Updated methods to handle LengthUnit instead of String.

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,76 +1,81 @@
 public class QuantityMeasurementApp {
 
+    // UC8: Refactoring Unit Enum to Standalone
+    // LengthUnit enum with conversion factors (base unit: inches)
+    enum LengthUnit {
+                INCHES(1.0),
+                FEET(12.0),
+                YARD(36.0);
+
+                private final double conversionFactor;
+
+                LengthUnit(double conversionFactor) {
+                                this.conversionFactor = conversionFactor;
+                }
+
+                public double getConversionFactor() {
+                                return conversionFactor;
+                }
+    }
+
+    // Refactored Quantity class using LengthUnit enum
     static class Quantity {
                 private double value;
-                private String unit;
+                private LengthUnit unit;
 
-            public Quantity(double value, String unit) {
+            public Quantity(double value, LengthUnit unit) {
                             this.value = value;
                             this.unit = unit;
             }
 
             public double getValue() { return value; }
-                public String getUnit() { return unit; }
+                public LengthUnit getUnit() { return unit; }
 
             public double toBaseUnit() {
-                            switch (unit.toLowerCase()) {
-                                case "feet":   return value * 12.0;
-                                case "inches": return value;
-                                case "yard":   return value * 36.0;
-                                default:       return value;
-                            }
+                            return value * unit.getConversionFactor();
             }
 
             public boolean isEqualTo(Quantity other) {
                             return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
             }
 
-            public Quantity convertTo(String targetUnit) {
+            public Quantity convertTo(LengthUnit targetUnit) {
                             double baseValue = this.toBaseUnit();
-                            double convertedValue;
-                            switch (targetUnit.toLowerCase()) {
-                                case "feet":   convertedValue = baseValue / 12.0; break;
-                                case "inches": convertedValue = baseValue; break;
-                                case "yard":   convertedValue = baseValue / 36.0; break;
-                                default:       convertedValue = baseValue;
-                            }
+                            double convertedValue = baseValue / targetUnit.getConversionFactor();
                             return new Quantity(convertedValue, targetUnit);
             }
 
-            // UC6: Addition returning in inches
             public Quantity add(Quantity other) {
-                            double totalInches = this.toBaseUnit() + other.toBaseUnit();
-                            return new Quantity(totalInches, "inches");
+                            double totalBase = this.toBaseUnit() + other.toBaseUnit();
+                            return new Quantity(totalBase, LengthUnit.INCHES);
             }
 
-            // UC7: Addition with Target Unit Specification
-            public Quantity addWithTargetUnit(Quantity other, String targetUnit) {
-                            double totalInches = this.toBaseUnit() + other.toBaseUnit();
-                            Quantity result = new Quantity(totalInches, "inches");
-                            return result.convertTo(targetUnit);
+            public Quantity addWithTargetUnit(Quantity other, LengthUnit targetUnit) {
+                            double totalBase = this.toBaseUnit() + other.toBaseUnit();
+                            double converted = totalBase / targetUnit.getConversionFactor();
+                            return new Quantity(converted, targetUnit);
             }
 
             @Override
                 public String toString() {
-                                return value + " " + unit;
+                                return value + " " + unit.name().toLowerCase();
                 }
     }
 
     public static void main(String[] args) {
-                // UC6: Addition
-            Quantity q1 = new Quantity(1.0, "feet");
-                Quantity q2 = new Quantity(12.0, "inches");
-                System.out.println("UC6: 1 foot + 12 inches = " + q1.add(q2));
+                // UC8: Using refactored LengthUnit enum
+            Quantity q1 = new Quantity(1.0, LengthUnit.FEET);
+                Quantity q2 = new Quantity(12.0, LengthUnit.INCHES);
+                System.out.println("UC8: 1 foot == 12 inches? " + q1.isEqualTo(q2));
 
-            // UC7: Addition with Target Unit Specification
-            Quantity qa = new Quantity(1.0, "feet");
-                Quantity qb = new Quantity(12.0, "inches");
-                System.out.println("UC7: 1 foot + 12 inches in feet = " + qa.addWithTargetUnit(qb, "feet"));
-                System.out.println("UC7: 1 foot + 12 inches in inches = " + qa.addWithTargetUnit(qb, "inches"));
-                System.out.println("UC7: 1 foot + 12 inches in yards = " + qa.addWithTargetUnit(qb, "yard"));
+            Quantity q3 = new Quantity(1.0, LengthUnit.YARD);
+                Quantity q4 = new Quantity(3.0, LengthUnit.FEET);
+                System.out.println("UC8: 1 yard == 3 feet? " + q3.isEqualTo(q4));
 
-            Quantity qc = new Quantity(2.0, "feet");
-                Quantity qd = new Quantity(1.0, "yard");
-                System.out.println("UC7: 2 feet + 1 yard in feet = " + qc.addWithTargetUnit(qd, "feet"));
+            Quantity converted = q1.convertTo(LengthUnit.INCHES);
+                System.out.println("UC8: 1 foot converted to inches = " + converted);
+
+            Quantity sum = q1.addWithTargetUnit(q2, LengthUnit.FEET);
+                System.out.println("UC8: 1 foot + 12 inches in feet = " + sum);
     }
 }
