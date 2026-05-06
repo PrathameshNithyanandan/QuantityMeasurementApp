6471487caa7e6509a3696c6d70b0e482f91d88be
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-05-06 09:22:34 +0530

    UC1: Feet Measurement Equality
    Added method to check equality of two feet measurements and tested it in main.

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,6 +1,23 @@
 public class QuantityMeasurementApp {
 
+    // UC1: Feet Measurement Equality
+    // Checks equality of two numerical values measured in feet
+    public boolean areFeetEqual(double feet1, double feet2) {
+                return Double.compare(feet1, feet2) == 0;
+    }
+
     public static void main(String[] args) {
-              System.out.println("Welcome to Quantity Measurement App");
+                QuantityMeasurementApp app = new QuantityMeasurementApp();
+
+            // UC1: Feet Measurement Equality
+            double feet1 = 3.0;
+                double feet2 = 3.0;
+                boolean result = app.areFeetEqual(feet1, feet2);
+                System.out.println("Are " + feet1 + " feet and " + feet2 + " feet equal? " + result);
+
+            double feet3 = 2.0;
+                double feet4 = 5.0;
+                boolean result2 = app.areFeetEqual(feet3, feet4);
+                System.out.println("Are " + feet3 + " feet and " + feet4 + " feet equal? " + result2);
     }
 }
